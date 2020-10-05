cf4521a79a5777ea8dd4d482ccbe4e0c99eabc36
obtuse-triangle <61399718+obtuse-triangle@users.noreply.github.com>
2020-10-05 23:00:38 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function position_scroll(scrollposition_1) {
+  var scrollPosition = $(scrollposition_1).offset().top;
 
+  $("html").animate(
+    {
+      scrollTop: scrollPosition,
+    },
+    500
+  );
+}
